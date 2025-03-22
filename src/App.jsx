b883bf7d6784b66256aa8b9a3884cd42b8dbdf56
bramiprudencio <bramiprudencio@gmail.com>
2025-03-22 10:36:32 -0400

    first search bar aproximation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Search & Selector</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input type="text" placeholder="What are you looking for" />
+        <select name="cars" id="cars">
+          <option value="volvo">All</option>
+          <option value="saab">Music</option>
+          <option value="mercedes">Movies</option>
+          <option value="audi">Series</option>
+        </select>
+        <button onClick={() => search(() => {
+          console.log('searching...')
+
+        })}>
+          Search
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
